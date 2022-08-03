4daa2c6d70b6b66cfece01cea44c2ea1e9f112ea
Nifemi Daniel <nifemidaniel50@gmail.com>
2022-08-03 14:57:35 -0700

    true caller application

--- src/africa/semicolon/trueCaller/controllers/UserController.java
+++ src/africa/semicolon/trueCaller/controllers/UserController.java
@@ -1,12 +1,14 @@
 package africa.semicolon.trueCaller.controllers;
 
-import africa.semicolon.trueCaller.data.models.Contact;
 import africa.semicolon.trueCaller.dto.requests.AddContactRequest;
 import africa.semicolon.trueCaller.dto.requests.RegisterRequest;
 import africa.semicolon.trueCaller.dto.responses.AddContactResponse;
 import africa.semicolon.trueCaller.dto.responses.AllConTactResponse;
 import africa.semicolon.trueCaller.dto.responses.RegisterResponse;
+import africa.semicolon.trueCaller.exceptions.UserExistException;
 import africa.semicolon.trueCaller.services.UserServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +18,13 @@ public class UserController {
     private final UserServiceImpl userService = new UserServiceImpl();
 
     @PostMapping("/user")
-    public RegisterResponse registerUser(@RequestBody RegisterRequest registerRequest) {
-        return userService.register(registerRequest);
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
+        try {
+            RegisterResponse serviceResponse = userService.register(registerRequest);
+            return new ResponseEntity<>(serviceResponse, HttpStatus.CREATED);
+        } catch (UserExistException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PatchMapping("/user")
